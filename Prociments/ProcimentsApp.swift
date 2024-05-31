ae747024a6e2dd701073da7b25546689287fc86b
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-05-31 16:44:09 +0200

    Initial Commit

---
+++ Prociments/ProcimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProcimentsApp.swift
+//  Prociments
+//
+//  Created by Maziar Layeghkar on 31.05.24.
+//
+
+import SwiftUI
+
+@main
+struct ProcimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
